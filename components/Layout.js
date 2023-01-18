4620bad9b03305da20a591de1305f6cbb25a2132
David Adiel <davidadiel96@gmail.com>
2023-01-19 00:04:12 +0700

    edit margin index

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 export default function Layout({ children }) {
   return (
-    <div className="mx-6 md:max-w-7xl md:mx-auto font-poppins">
+    <div className="mx-6 md:max-w-3xl md:mx-auto lg:max-w-4xl font-poppins">
       <Nav />
       <main>{children}</main>
     </div>
